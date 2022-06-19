16b54bf52e52fbd11714d4e86ef3fa503449207a
alexkallai <scythe1092@gmail.com>
2022-06-19 10:31:49 +0200

    Added description and removed printing

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # batch-subtitle-renamer
-This tool renames all subtitles in a given folder to the video file names by matching the season and episode numberings
+This tool renames all subtitles in a given folder to the video file names
+
+# Usage:
+python main.py "folder where the video files and subtitles are located"
+e.g.:
+python main.py "c:\tmp\testfolder\"
+
+e.g.: If a folder contains the following files:
+House.of.Cards.2013.S02E07.720p.BluRay.x264-DEMAND.srt
+House.of.Cards.2013.S02E07.1080p.BluRay.x265-RARBG.mp4
+
+after running to tool it will look like so:
+House.of.Cards.2013.S02E07.1080p.BluRay.x265-RARBG.srt
+House.of.Cards.2013.S02E07.1080p.BluRay.x265-RARBG.mp4
+
+the matching is done by the season and episode codes, so the S01E01 will match with s01e01
+though not all combinations are taken into account, so s01e01 will NOT match with s1e1 notation
--- main.py
+++ main.py
@@ -49,9 +49,6 @@ def main():
         onlyfiles = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
 
         videoFileList, subtitleFileList = splitFilesList(onlyfiles)
-        print(videoFileList)
-        print(subtitleFileList)
-        print("*******************")
 
         for videoFileName in videoFileList:
                 # Get the S01E12 part from the file and search for it in the subtitle names
@@ -59,19 +56,15 @@ def main():
                 # explanation: starts with ., continues with s or S, and 1 or 2 numbers, then e or E, and 1 or 2 numbers, and then a .
                 # not using lower because the video file names should be kept the same, maybe they are still being downloaded...
                 seasonEpisodeIdentifier = re.search(r".[sS]\d{1,2}[eE]\d{1,2}.", videoFileName)
-                print(seasonEpisodeIdentifier.group(0))
                 seasonEpisodeIdentifierText = seasonEpisodeIdentifier.group(0)
 
                 for subtitleFileName in subtitleFileList:
                         if seasonEpisodeIdentifierText.lower() in subtitleFileName.lower():
                                 # Rename that subtitle file to the video file name + the original extension
                                 nameWithoutExtension, subtitleExtension = splitext(subtitleFileName)
-                                print(nameWithoutExtension)
                                 oldSubTitleFileName = join(folderPath, subtitleFileName)
                                 videoFileNameWithoutExtension, videoExtension = splitext(videoFileName)
                                 newSubTitleFileName = join(folderPath, videoFileNameWithoutExtension + subtitleExtension)
-                                print(oldSubTitleFileName)
-                                print(newSubTitleFileName)
                                 rename(oldSubTitleFileName, newSubTitleFileName)
                                 break
 
